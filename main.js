4545ebc43193f46d139f1bda3bb540965b104fd5
rmleach <ryan.leach@mac.com>
2020-03-16 22:08:23 +0000

    run button

---
+++ main.js
@@ -0,0 +1,9 @@
+// Given an array of unsorted integers, return the mean, median, and mode.//
+const arr = [29, 24, 98, 74, 6, 20, 0, 98, 2, 4]
+
+
+const mean = (arr) => {console.log('mean')}
+const median = () => {}
+const mode = () => {}
+
+console.log(mean)
